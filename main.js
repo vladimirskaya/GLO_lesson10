c237fe390facbacd22e6bc90698f17edbf282422
vladimirskaya <63165081+vladimirskaya@users.noreply.github.com>
2021-02-11 03:43:19 +0300

    feat: edit file, add comment

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
 'use strict'
 
+//get node-parent and node-children
 let booksContainer = document.getElementsByClassName('books')[0];
 let booksContainerItems = booksContainer.getElementsByClassName('book');
-console.log(booksContainer, booksContainerItems);
+//console.log(booksContainer, booksContainerItems);
 
-//let div = document.createElement(div);
-//div.innerHTML = "Here I am";
-//
+//delete advertising window
 document.getElementsByClassName('adv')[0].remove();
 
+//getting correct order of node-children
 booksContainer.prepend(booksContainerItems[1]);
 booksContainerItems[2].remove;
 booksContainer.append(booksContainerItems[2]);
 booksContainerItems[1].after(booksContainerItems[3]);
 
-
+//change body background image
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 console.log(document.body.style.backgroundImage);
 
+//edit title of the node-child
 let bContItemTitle = booksContainerItems[2].getElementsByTagName('a')[0];
 bContItemTitle.innerHTML = 'Книга 3. this и Прототипы Объектов';
 console.log(booksContainerItems[2], bContItemTitle);
 
-const  chaptersOfTitleItems = booksContainerItems[1].getElementsByTagName('li');
-console.log(chaptersOfTitleItems);
-
-//console.log(booksContainerItems[1].getElementsByTagName('ul'));
-//booksContainerItems[1].getElementsByTagName('ul')[0].remove();
-
-
-//let chapters = Array.from(booksContainerItems[1].getElementsByTagName('li'));
-
-let intro1 = chaptersOfTitleItems[0],
+// do proper order of nodes-children 
+let  	chaptersOfTitleItems = booksContainerItems[1].getElementsByTagName('li'),
+	intro1 = chaptersOfTitleItems[0],
 	intro2 = chaptersOfTitleItems[1],
 	chapter1 = chaptersOfTitleItems[3],
 	chapter2 = chaptersOfTitleItems[6],
@@ -51,10 +45,6 @@ chapter4.after(chapter5);
 chapter5.after(annexesA);
 annexesA.after(annexesB);
 annexesB.after(annexesC);
-//chapter1.after(chapter2);
-//console.log("sds", chapters);
-//chaptersOfTitleItems[3].after(chaptersOfTitleItems[2]);
-//chaptersOfTitleItems[6].after(chaptersOfTitleItems[2]);
 
 
 
